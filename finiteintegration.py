2c36934c19d96276830e262aabb50583227985aa
Travis Martin <nitramsivart@gmail.com>
2012-03-27 15:32:05 -0400

    simplified integration

--- finiteintegration.py
+++ finiteintegration.py
@@ -1,4 +1,4 @@
-from math import sin, exp
+from math import sin, exp, tan, atanh, pi, cos, floor
 from numpy import arange, zeros
 from pylab import plot,xlabel,ylabel,show
 from multiprocessing import Pool
@@ -6,61 +6,101 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import sys
 
-def B(d):
+def map_fn(theta):
+  return theta
+  return tan(theta)
+
+def d_map_fn(theta):
+  return 1
+  return theta
+  return 1/(cos(theta)**2)
+
+def C(d):
   alpha = 8
   r_0 = 1
   c = 1
   r = 1
   p = (1 + (abs(d)*r_0)**(alpha))**(-1)
   e = c*exp(-r * abs(d))
+
+  if abs(d) < .00001:
+    return 1
+  else:
+    return 0
+
   g = exp(-1 * (d**2) / 2)
 
   return g
 
+def compute_coeff(index, size):
+  if index is 0 or index is (size - 1):
+    return 1
+  elif index % 2 is 0:
+    return 2
+  else:
+    return 4
 
 # This is the integral from 0 to N of:
 # B(x-x')u(x',t) dx'
-def integral(u):
+def integral(u, beta):
   ret = u.copy()
-  v = 1.85
   flag = True
 
-  for currentx in range(xsize):
+  for currenty in range(thetasize):
     #this total is the sum up to currentx of the whole formula
     total = 0.0
 
-    for x in range(currentx):
+    for y in range(currenty):
       #this total is the sum of the inner integral
       total2 = 0.0
-      xprimesize = xsize
-      xprimeresolution = xresolution 
+      yprimesize = thetasize
+      yprimeresolution = thetaresolution
   
-      for xprime in range(xprimesize):
-      
-        if xprime is 0 or xprime is (xprimesize - 1):
-          coefficient2 = 1
-        elif xprime % 2 is 0:
-          coefficient2 = 2
-        else:
-          coefficient2 = 4
+      for yprime in range(yprimesize):
+        coeff2 = compute_coeff(yprime, yprimesize)
+        total2 += coeff2 * (beta(map_fn(thetavals[y])-map_fn(thetavals[yprime])) * u[yprime]) * d_map_fn(thetavals[yprime])
+      total2 = total2 * yprimeresolution / 3
 
-  
-        total2 = total2 + coefficient2 * (B(x*xresolution-xprime*xprimeresolution) * u[xprime])
-      total2 = total2 * xprimeresolution / 3
+      coefficient = compute_coeff(y, thetasize)
+      total += coefficient * (1 - u[y]) * total2 * d_map_fn(thetavals[y])
 
-      if x is 0 or x is (xsize - 1):
-        coefficient = 1
-      elif xprime % 2 is 0:
-        coefficient = 2
-      else:
-        coefficient = 4
-      total += coefficient * (1 - u[x]) * total2
-
-    total = total * xresolution / 3
-    ret[currentx] = -total/v + 1
-    if abs(ret[currentx] - u[currentx]) > 1 and flag:
+    total = total * thetaresolution / 3
+    ret[currenty] = -total
+    if abs(ret[currenty] - u[currenty]) > 1 and flag:
       flag = False
       print "ERROR TOO BIG CHANGE"
+  #we need to satisfy the conditions that f(-infty) = 1, f(infty) = 0, f(0) = 1/2
+  #first scale ret
+  v = abs(ret[len(ret)-1])
+  print "Velocity: " + str(v)
+  if v <= 0:
+    return ret
+  ret = ret/v
+  #then shift ret upward
+  ret = ret + 1
+  #then shift sideways
+  middle_index = 0
+  expected_middle_index = floor(len(ret)/2)
+  for i, v in enumerate(ret):
+    if v < .5:
+      middle_index = i
+      break
+  difference = middle_index - expected_middle_index
+  print "Difference: %d" % difference
+  if False and difference < 0:
+    newret = ret.copy()
+    for i, v in enumerate(ret):
+      if i+difference < 0:
+        newret[i] = 1
+      else:
+        newret[i] = ret[i+difference]
+    ret = newret
+  elif False:
+    for i, v in enumerate(ret):
+      if i+difference >= len(ret):
+        ret[i] = 0
+      else:
+        ret[i] = ret[i+difference]
   return ret
 
 def init_line():
@@ -72,14 +112,16 @@ def update_line(num, data, line):
   line.set_data(xvals, upoints[int(seconds/tresolution)])
   return line,
 
-xresolution = .5
+thetaresolution = .3
 tresolution = 1
 
 tstart = 0.0
-tend = 3
+tend = 5
 
-xstart = -30
-xend = 30
+thetastart = -pi/2
+thetaend = pi/2
+thetastart = -20
+thetaend = 20
 
 '''
 if(len(sys.argv) > 4):
@@ -93,9 +135,9 @@ if(len(sys.argv) > 1):
 '''
 
 # the x-range of our algorithm
-xsize = int((xend-xstart)/xresolution + 1)
-xpoints = arange(xsize)
-xvals = list(float(x)*xresolution + xstart for x in range(xsize))
+thetasize = int((thetaend-thetastart)/thetaresolution + 1)
+thetapoints = arange(thetasize)
+thetavals = list(float(x)*thetaresolution + thetastart for x in range(thetasize))
 
 
 tsize = int((tend-tstart)/tresolution + 1)
@@ -103,48 +145,70 @@ tpoints = arange(tsize - 1)
 tvals = list(float(t)*tresolution + tstart for t in range(tsize))
 
 
-upoints = zeros([tsize,xsize])
+upoints = zeros([tsize,thetasize])
+
 
 # set the initial conditions,
 # only one person infected
-for i in range(xsize):
+for i in range(thetasize):
   #upoints[0][i] = 1 if xvals[i] < 0 else 0
-  upoints[0][i] = 1.0 - 1.0/(1.0+exp(-xvals[i]))
+  power = map_fn(thetavals[i])
+  cutoff = 500
+  if power > cutoff:
+    print "TOO BIG"
+    power = cutoff
+  elif power < -cutoff:
+    print "TOO SMALL"
+    power = -cutoff
+  upoints[0][i] = 1.0 - 1.0/(1.0+exp(-power))
 
-
-for t in tpoints:
+for t in tpoints[1:len(tpoints)]:
   print (t*tresolution)
 
-  first = 0
-  last = xsize-1
-  margin = .1
-  for x in xpoints:
-    if upoints[t][x] > 1-margin:
-      first = x
-    elif upoints[t][x] < margin and last == xsize - 1:
-      last = x
-  width = (last - first)
-  reduction_factor = int(width/11)+1
-  print "Reduction factor: %d" % reduction_factor
+  u = upoints[t-1]
+  newu = integral(u, C)
+  upoints[t] = newu
 
-  u = upoints[t]
-  newu = integral(u)
+  plt.figure(t)
+  plt.xlabel('X val')
+  plt.title('Time graph of wave, iteration #' + str(t))
 
-  if((t+1) != tsize):
-    upoints[t+1] = newu
+  x = thetavals
+  y = upoints[t]
 
-fig = plt.figure()
-#l, = plt.plot(xvals, upoints[0], 'r-')
-plt.xlim(xstart, xend)
-plt.ylim(0, 1)
-plt.xlabel('x')
-plt.title('Time graph of wave')
-#line_ani = animation.FuncAnimation(fig, update_line, tsize, init_line, fargs=(upoints, l), interval=tresolution*500, blit=True)
-for t in tpoints:
-  plt.plot(xvals, upoints[t])
-  plt.show()
+  plt.scatter(x, y)
 
 '''
+#We ignore any data outside of these bounds, for display purposes
+xmin = -1000
+xmax = 1000
+
+min_index = 0
+max_index = thetasize - 1
+
+for i in range(thetasize):
+  if map_fn(thetavals[i]) < xmax:
+    max_index = i
+  if map_fn(thetavals[i]) > xmin and min_index == 0:
+    min_index = i
+
+print "Min: %d, Max: %d" % (min_index, max_index)
+print "Total Min: %d, Total Max: %d" % (0, thetasize-1)
+
+#l, = plt.plot(xvals, upoints[0], 'r-')
+plt.xlabel('theta')
+plt.title('Time graph of wave, delta')
+#line_ani = animation.FuncAnimation(fig, update_line, tsize, init_line, fargs=(upoints, l), interval=tresolution*500, blit=True)
+
+  plt.figure(t)
+  plt.xlabel('X val')
+  plt.title('Time graph of wave, iteration #' + str(t))
+
+  x = list(map_fn(v) for v in thetavals[min_index:max_index])
+  y = upoints[t][min_index:max_index]
+
+  plt.scatter(x, y)
+plt.show()
 plt.figure(2)
 plt.xlim(xstart, xend)
 plt.plot(xvals, upoints[1], 'r-')
