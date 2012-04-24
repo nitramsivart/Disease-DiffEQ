5d58852daa4d782a85a95eeef693e4b60cabfe0d
Travis Martin <nitramsivart@gmail.com>
2012-04-24 10:14:13 -0400

    working dynamical systems

--- dynamicalsystems.py
+++ dynamicalsystems.py
@@ -15,7 +15,13 @@ def d_map_fn(theta):
   return 1/(cos(theta)**2)
 
 def gaussian1(d):
-  return exp(-1 * ((.125*d)**2) / 2)
+  return exp(-1 * ((d)**2) / 2)
+
+def exp1(d):
+  return exp(-1 * ( d / 2))
+
+def exp2(d):
+  return exp(-1 * (.5 * d / 2))
 
 def gaussian2(d):
   return exp(-1 * ((.25*d)**2) / 2)
@@ -47,12 +53,14 @@ def compute_coeff(index, size):
 def compute_deriv(f, xindex, xwidth):
   if xindex == (len(f) - 1):
     return (f[xindex] - f[xindex - 1])/xwidth
-  else:
+  elif xindex == 0:
     return (f[xindex + 1] - f[xindex])/xwidth
+  else:
+    return (f[xindex + 1] - f[xindex - 1]) / (2 * xwidth)
 
 # This is the integral from 0 to N of:
 # B(x-x')u(x',t) dx'
-def integral(u, beta):
+def integral(u, beta, v):
   change = u.copy()
   flag = True
 
@@ -70,23 +78,23 @@ def integral(u, beta):
     deriv = compute_deriv(u, currenty, thetaresolution)
 
     #calculate right side
-    v = 140.0
-    right_side = -1/v * (1 - u[currenty]) * conv[currenty] * d_map_fn(thetavals[currenty])
+    right_side = -1/float(v) * (1 - u[currenty]) * conv[currenty] * d_map_fn(thetavals[currenty])
 
     #calculate df/dt
     change[currenty] = tresolution * (deriv - right_side)
 
   return change 
 
-def do_runge_kutta(u, beta):
-  k1 = integral(u, beta)
-  k2 = integral(u+.5*k1, beta)
-  k3 = integral(u+.5*k2, beta)
-  k4 = integral(u+k3, beta)
+def do_runge_kutta(u, beta, v):
+  k1 = integral(u, beta, v)
+  k2 = integral(u+.5*k1, beta, v)
+  k3 = integral(u+.5*k2, beta, v)
+  k4 = integral(u+k3, beta, v)
 
   ret = u.copy()
   ret += (k1+2*k2+2*k3+k4)/6
 
+  return ret
   return rescale(ret)
 
 def rescale(ret):
@@ -125,15 +133,14 @@ def rescale(ret):
         ret[i] = ret[i+difference]
   return ret
 
-thetaresolution = .5
-tresolution = .25 
+thetaresolution = .3
+tresolution = .1
 
 tstart = 0.0
-tend = 25
+tend = 30
 
-thetastart = -40
-thetaend = 40
-
+thetastart = -15
+thetaend = 15
 
 # the x-range of our algorithm
 thetasize = int((thetaend-thetastart)/thetaresolution + 1)
@@ -141,8 +148,8 @@ thetapoints = arange(thetasize)
 thetavals = list(float(x)*thetaresolution + thetastart for x in range(thetasize))
 
 #We ignore any data outside of these bounds, for display purposes
-xmin = -100
-xmax = 100
+xmin = -200
+xmax = 200
 
 min_index = 0
 max_index = thetasize - 1
@@ -170,13 +177,20 @@ upoints3 = zeros([tsize,thetasize])
 upoints4 = zeros([tsize,thetasize])
 upoints5 = zeros([tsize,thetasize])
 
-def init_line():
-  l.set_data([], [])
-  return l,
+def init_line1():
+  l1.set_data([], [])
+  return l1,
 
-def update_line(num, data, line):
-  print "tick"
+def update_line1(num, data, line):
   line.set_data(thetavals, upoints1[num])
+  return line,
+
+def init_line2():
+  l2.set_data([], [])
+  return l2,
+
+def update_line2(num, data, line):
+  line.set_data(thetavals, upoints2[num])
   return line,
 
 # set the initial conditions,
@@ -184,8 +198,13 @@ def update_line(num, data, line):
 for i in range(thetasize):
   power = map_fn(thetavals[i])
 
-  upoints1[0][i] = 1.0 - 1.0/(1.0+exp(-power/5))
-  upoints2[0][i] = 1.0 - 1.0/(1.0+exp(-power/4))
+  upoints1[0][i] = 1.0 - 1.0/(1.0+exp(-power))
+  upoints1[0][i] = 1.0 - float(i)/(thetasize - 1)
+  if abs(power) < .1:
+    upoints1[0][i] = .1 - abs(power)
+  else:
+    upoints1[0][i] = 0
+  upoints2[0][i] = 1.0 - 1.0/(1.0+exp(-power))
   upoints3[0][i] = 1.0 - 1.0/(1.0+exp(-power/3))
   upoints4[0][i] = 1.0 - 1.0/(1.0+exp(-power/2))
   upoints5[0][i] = 1.0 - 1.0/(1.0+exp(-power/1.5))
@@ -194,11 +213,11 @@ for t in tpoints[1:len(tpoints)]:
   print (t*tresolution)
 
   u = upoints1[t-1]
-  newu = do_runge_kutta(u, gaussian1)
+  newu = do_runge_kutta(u, gaussian1, 50)
   upoints1[t] = newu
 
   u = upoints2[t-1]
-  #newu = integral(u, gaussian2)
+  #newu = do_runge_kutta(u, gaussian1, 140)
   upoints2[t] = newu
 
   u = upoints3[t-1]
@@ -213,10 +232,17 @@ for t in tpoints[1:len(tpoints)]:
   #newu = integral(u, gaussian5)
   upoints5[t] = newu
 
-fig = plt.figure(0)
+fig1 = plt.figure(0)
 plt.xlabel('X val')
-l, = plt.plot(thetavals, upoints1[0], 'r')
-line_ani = animation.FuncAnimation(fig, update_line, tsize, init_line, fargs=(upoints1, l), interval=tresolution*30, blit=True)
+plt.title('Time graph of wave' + str(thetaresolution) + ',' + str(tresolution) + ',' + str(tend))
+l1, = plt.plot(thetavals, upoints1[0], 'r')
+line_ani = animation.FuncAnimation(fig1, update_line1, tsize, init_line1, fargs=(upoints1, l1), interval=tresolution*100, blit=True)
+
+#fig2 = plt.figure(1)
+plt.xlabel('X val')
+plt.title('Time graph of wave' + str(thetaresolution) + ',' + str(tresolution) + ',' + str(tend))
+#l2, = plt.plot(thetavals, upoints2[0], 'b')
+#line_ani2 = animation.FuncAnimation(fig2, update_line2, tsize, init_line2, fargs=(upoints2, l2), interval=tresolution*50, blit=True)
 
 '''
 for t in tpoints:
@@ -233,14 +259,15 @@ for t in tpoints:
   #plt.scatter(x3, upoints3[t], color = 'g')
   #plt.scatter(x, upoints4[t], color = 'k')
   #plt.scatter(x5, upoints5[t], color = 'y')
+'''
 plt.figure(len(tpoints))
 plt.xlabel('Distance')
 plt.title('Beta functions')
 
-plt.scatter(x1, list(gaussian1(map_fn(x)) for x in thetavals), color = 'b')
-#plt.scatter(x, list(gaussian2(map_fn(x)) for x in thetavals), color = 'r')
+plt.scatter(thetavals, list(gaussian1(abs(map_fn(x))) for x in thetavals), color = 'b')
+plt.scatter(thetavals, list(gaussian1(abs(map_fn(x))) for x in thetavals), color = 'r')
 #plt.scatter(x1, list(gaussian3(map_fn(x)) for x in thetavals), color = 'g')
 #plt.scatter(x, list(gaussian4(map_fn(x)) for x in thetavals), color = 'k')
 #plt.scatter(x1, list(gaussian5(map_fn(x)) for x in thetavals), color = 'y')
-'''
+
 plt.show()
